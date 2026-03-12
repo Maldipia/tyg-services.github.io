7f6bd22019bc974ec9a47076401d1893ecdd8c91
Maldipia <tygfsb@gmail.com>
2026-03-12 10:36:13 +0800

    chore: activate Google Apps Script webhook integration

---
+++ next.config.js
@@ -0,0 +1,7 @@
+{
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest/repos/contents#get-repository-content",
+  "status": "404"
+}
+
+// GAS webhook activated: 2026-03-12
